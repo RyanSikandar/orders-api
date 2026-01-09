2ea1d7ec7175540c0a652bc29d52c20ead38ff94
Ryan Sikandar <ryansikandar@gmail.com>
2026-01-10 00:37:06 +0500

    chore: add logger

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	router := chi.NewRouter()
+	router.Use(middleware.Logger)
 
 	router.Get("/", basicHandler)
 
